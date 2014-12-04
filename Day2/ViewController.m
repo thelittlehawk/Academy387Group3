2d9ab729d2ba25d0fcf92535b17f9b351d4abdb6
thelittlehawk <faik.catibusic@live.com>
2014-12-04 17:47:51 +0100

    Init Message

---
+++ Day2/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  Day2
+//
+//  Created by Faik Catibusic on 12/3/14.
+//  Copyright (c) 2014 Academy387. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)saveClicked:(id)sender {
+    NSString *firstName = self.firstNameTextField.text;
+    NSString *lastName = self.lastNameTextField.text;
+    
+    NSString *fullName = [[NSString alloc] initWithFormat:@"%@, %@", firstName, lastName];
+    
+    self.resultTextField.text = fullName;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
